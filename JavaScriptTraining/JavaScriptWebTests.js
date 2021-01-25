f86a2aac26fafd2782a5d89fe6335e04d4519133
TheCatIsOnTheMoon <77201994+TheCatIsOnTheMoon@users.noreply.github.com>
2021-01-25 17:44:05 +0100

    training JS of the day

--- JavaScriptTraining/JavaScriptWebTests.js
+++ JavaScriptTraining/JavaScriptWebTests.js
@@ -12,3 +12,13 @@ const thirdArticle = articles[2];
 
 //complexe research in the DOM
 const elt = document.querySelector("#myId p.article > a");
+
+// ocr exercise 2 ======================================
+const newElt = document.createElement('p');
+let eltMain = document.getElementById('main');
+
+eltMain.appendChild(newElt);
+
+newElt.innerHTML = 'Mon <strong>grand</strong> contenu';
+newElt.classList.add('important');
+newElt.style.color = 'green';
