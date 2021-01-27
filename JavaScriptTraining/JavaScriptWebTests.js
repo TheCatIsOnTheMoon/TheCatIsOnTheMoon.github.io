8290201128096181ca5d48965d6297bdf71e4a63
TheCatIsOnTheMoon <77201994+TheCatIsOnTheMoon@users.noreply.github.com>
2021-01-27 16:52:05 +0100

    training of the day

--- JavaScriptTraining/JavaScriptWebTests.js
+++ JavaScriptTraining/JavaScriptWebTests.js
@@ -22,3 +22,43 @@ eltMain.appendChild(newElt);
 newElt.innerHTML = 'Mon <strong>grand</strong> contenu';
 newElt.classList.add('important');
 newElt.style.color = 'green';
+
+// ocr exercice 3 : Ecouter des evenements ==========================
+
+const parent_article = document.getElementById('parent');
+const child_a = document.getElementById('child');
+const parent_count = document.getElementById('parent-count');
+const child_count = document.getElementById('child-count');
+let parent_score = 0;
+let child_score = 0;
+
+//======================
+
+function addScore(label) {
+  switch (label) {
+    case 'parent':
+      parent_score++;
+      parent_count.innerHTML = parent_score;
+    break
+    case 'child':
+      child_score++;
+      child_count.innerHTML = child_score;
+    break
+}}
+
+//==================
+
+function main () {
+parent_article.addEventListener('click', function() {
+  addScore('parent');
+});
+
+child_a.addEventListener('click', function(event) {
+  event.stopPropagation();
+  addScore('child');
+});
+}
+
+main();
+
+// =====================================================================
